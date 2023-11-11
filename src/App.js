e437b5ab46c9685b184469f991d0afb9d2e0bd11
MAR-Rehman <murad_rehman@hotmail.com>
2023-11-11 16:47:18 +0000

    on app.js, changed urls for new backend urls

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://api.render.com/deploy/srv-cl7ql4ivokcc73ap7qn0?key=w6WzVnCH4U0', {
               method: 'post',
               headers: {'Content-Type': 'application/json'},
               body: JSON.stringify({
@@ -88,7 +88,7 @@ class App extends Component {
             .then(response => {
               console.log('hi', response)
               if (response) {
-                fetch('http://localhost:3000/image', {
+                fetch('https://api.render.com/deploy/srv-cl7ql4ivokcc73ap7qn0?key=w6WzVnCH4U0', {
                   method: 'put',
                   headers: {'Content-Type': 'application/json'},
                   body: JSON.stringify({
